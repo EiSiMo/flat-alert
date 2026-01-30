047f8a2a318b583b135db5977c962622f29a04db
Moritz <moritz.eigenauer@gmail.com>
2026-01-31 00:23:04 +0100

    simplify

--- telegram.py
+++ telegram.py
@@ -6,14 +6,10 @@ from settings import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 logger = logging.getLogger("flat-alert (telegram.py)")
 
 class Telegram:
-    def __init__(self):
-        self.token = TELEGRAM_TOKEN
-        self.chat_id = TELEGRAM_CHAT_ID
-
     def send_message(self, msg):
-        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
         payload = {
-            "chat_id": self.chat_id,
+            "chat_id": TELEGRAM_CHAT_ID,
             "text": msg,
             "parse_mode": "Markdown",
             "disable_web_page_preview": True
