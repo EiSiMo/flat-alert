c345464695bf36d8c90c3c5d77d10344eba1a5fd
Moritz <moritz.eigenauer@gmail.com>
2026-02-02 17:10:55 +0100

    unify settings with flat-apply project

--- settings.py
+++ settings.py
@@ -1,13 +1,14 @@
 from dotenv import load_dotenv
 from os import getenv
 
-LANGUAGE: str = "de"
-TIME_INTERVALL = 60
+load_dotenv()
+
+LANGUAGE: str = getenv("LANGAUGE", "en")
+TIME_INTERVALL: int = int(getenv("SLEEP_INTERVALL", "60"))
 
 # secrets
-load_dotenv()
-GMAPS_API_KEY = getenv("GMAPS_API_KEY")
-TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = getenv("TELEGRAM_CHAT_ID")
-BERLIN_WOHNEN_USERNAME = getenv("BERLIN_WOHNEN_USERNAME")
-BERLIN_WOHNEN_PASSWORD = getenv("BERLIN_WOHNEN_PASSWORD")
+GMAPS_API_KEY: str = getenv("GMAPS_API_KEY")
+TELEGRAM_TOKEN: str = getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID: str = getenv("TELEGRAM_CHAT_ID")
+BERLIN_WOHNEN_USERNAME: str = getenv("BERLIN_WOHNEN_USERNAME")
+BERLIN_WOHNEN_PASSWORD: str = getenv("BERLIN_WOHNEN_PASSWORD")
